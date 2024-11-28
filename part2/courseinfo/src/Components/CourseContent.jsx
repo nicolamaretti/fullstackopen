881332cb84c09156fe9af0dec8510cfb488d6dd8
Nicola Maretti <nico.maretti@gmail.com>
2024-11-29 00:18:56 +0100

    exercise 2.2

--- part2/courseinfo/src/Components/CourseContent.jsx
+++ part2/courseinfo/src/Components/CourseContent.jsx
@@ -1,15 +1,22 @@
 import CoursePart from "./CoursePart";
 
 const CourseContent = ({ parts }) => {
+	let exercisesTotal = 0;
+
 	return (
 		<div>
-			{parts.map((part) => (
-				<CoursePart
-					key={part.id}
-					name={part.name}
-					exercises={part.exercises}
-				/>
-			))}
+			{parts.map((part) => {
+				exercisesTotal += part.exercises;
+
+				return (
+					<CoursePart
+						key={part.id}
+						name={part.name}
+						exercises={part.exercises}
+					/>
+				);
+			})}
+			<p style={{ fontWeight: 700 }}>total of {exercisesTotal} exercises</p>
 		</div>
 	);
 };
